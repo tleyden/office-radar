c61804999449274ba39f0423b94ed096608bb7e2
Traun Leyden <tleyden@couchbase.com>
2014-07-01 10:43:21 -0700

    Add titles

---
+++ sync_gateway/sample_docs/post.sh
@@ -0,0 +1,3 @@
+
+curl -v -X POST -H "Content-type: application/json" -d '{"type":"beacon","uuid":"08D4A950-80F0-4D42-A14B-D53E063516E6", "major":61001, "minor":13001, "organization":"couchbase", "location":"mobile", "desc":"Trauns Macbook Virtual Beacon"}' http://162.222.178.49:4984/office_radar/
+
