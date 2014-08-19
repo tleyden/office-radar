f21c5ec07511d297f021d228cbd98d62c40ebbfc
Traun Leyden <tleyden@couchbase.com>
2014-08-19 10:28:25 -0700

    Use new demo cluster url

--- ios/OfficeRadar/OfficeRadar/RDConstants.m
+++ ios/OfficeRadar/OfficeRadar/RDConstants.m
@@ -4,7 +4,8 @@
 
 @implementation RDConstants
 
-NSString *const kSyncURL = @"http://162.222.178.49:4984/office_radar";
+NSString *const kSyncURL = @"http://demo.sc.couchbase.com/officeradar";
+
 NSString *const kDocType = @"type";
 NSString *const kDocTypeBeacon = @"beacon";
 NSString *const kFieldUuid = @"uuid";
